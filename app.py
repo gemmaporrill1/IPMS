329578e757018d9037b997ea5b726cd6fd394031
gemmaporrill1 <gemma.porrill@sanlam.co.za>
2024-03-27 08:07:50 +0200

    Progress on changing over to the database

--- app.py
+++ app.py
@@ -1,66 +1,104 @@
+import os
+import uuid
 from flask import Flask, jsonify, request, render_template
-
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+
+print(os.environ.get("AZURE_DATABASE_URL"))
 
 app = Flask(__name__, static_folder="static")
 
-policies = [
-    {
-        "policyID": 1,
-        "name": "Vehicle",
-        "description": "Vehicle insurance provides you financial protection against damages, theft, or liability related to operating a vehicle. Costs vary based on factors like coverage type, vehicle value, driver history, and location.",
-        "premium": 99,
-        "length": 2,
-        "payOut": 50000,
-    },
-    {
-        "policyID": 2,
-        "name": "Property",
-        "description": "Property insurance safeguards real estate against damages from various perils like fire, theft, and natural disasters, including liability coverage. Costs vary based on property value, location, coverage type, and deductible, ranging from hundreds to thousands annually.",
-        "premium": 300,
-        "length": 10,
-        "payOut": 800000,
-    },
-    {
-        "policyID": 3,
-        "name": "Life Insurance",
-        "description": "Life insurance provides a lump-sum payment to beneficiaries upon the policyholder's death. Costs depend on factors like age, health, coverage amount, and policy type, varying from low monthly payments to higher premiums.",
-        "premium": 159,
-        "length": 50,
-        "payOut": 2000000,
-    },
-]
 
-customers = [
-    {
-        "id": 1,
-        "name": "John Smith",
-        "email": "johnsmith@email.com",
-        "policy": "Health",
-        "startDate": "2020-01-01",
-    },
-    {
-        "id": 2,
-        "name": "Jane Smith",
-        "email": "janesmith@email.com",
-        "policy": "Vehicle",
-        "startDate": "2021-04-01",
-    },
-    {
-        "id": 3,
-        "name": "Doug Brown",
-        "email": "dougbrown@email.com",
-        "policy": "Health",
-        "startDate": "2018-12-01",
-    },
-    {
-        "id": 4,
-        "name": "Henry Pants",
-        "email": "hp12@email.com",
-        "policy": "Life",
-        "startDate": "2023-01-01",
-    },
-]
+connection_string = os.environ.get("AZURE_DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = connection_string
+db = SQLAlchemy(app)
+
+try:
+    with app.app_context():
+        # Use text() to explicitly declare your SQL command
+        result = db.session.execute(text("SELECT 1")).fetchall()
+        print("Connection successful:", result)
+except Exception as e:
+    print("Error connecting to the database:", e)
+
+
+class Policy(db.Model):
+    __tablename__ = "policies"
+    policyID = db.Column(
+        db.String(50), primary_key=True, default=lambda: str(uuid.uuid4())
+    )
+    name = db.Column(db.String(100))
+    description = db.Column(db.String(500))
+    premium = db.Column(db.Float)
+    length = db.Column(db.Integer)
+    payout = db.Column(db.Integer)
+
+    # JSON = keys
+    def to_dict(self):
+        return {
+            "policyID": self.policyID,
+            "name": self.name,
+            "description": self.description,
+            "premium": self.premium,
+            "length": self.length,
+            "payout": self.payout,
+        }
+
+
+class Customer(db.Model):
+    __tablename__ = "customers"
+    customerID = db.Column(
+        db.String(50), primary_key=True, default=lambda: str(uuid.uuid4())
+    )
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(50))
+    startDate = db.Column(db.DateTime)
+    policyID = db.Column(db.String(50), db.ForeignKey("policies.policyID"))
+
+    def to_dict(self):
+        return {
+            "customerID": self.customerID,
+            "name": self.name,
+            "email": self.email,
+            "startDate": self.startDate,
+            "policyID": self.policyID,
+        }
+
+
+# customers = [
+#     {
+#         "id": 1,
+#         "name": "John Smith",
+#         "email": "johnsmith@email.com",
+#         "policy": "Health",
+#         "startDate": "2020-01-01",
+#     },
+#     {
+#         "id": 2,
+#         "name": "Jane Smith",
+#         "email": "janesmith@email.com",
+#         "policy": "Vehicle",
+#         "startDate": "2021-04-01",
+#     },
+#     {
+#         "id": 3,
+#         "name": "Doug Brown",
+#         "email": "dougbrown@email.com",
+#         "policy": "Health",
+#         "startDate": "2018-12-01",
+#     },
+#     {
+#         "id": 4,
+#         "name": "Henry Pants",
+#         "email": "hp12@email.com",
+#         "policy": "Life",
+#         "startDate": "2023-01-01",
+#     },
+# ]
 
 
 @app.route("/")
@@ -75,21 +113,20 @@ def home_page():
 
 @app.route("/customer", methods=["GET", "POST"])
 def customer_management():
+    customers = Customer.query.all()
     if request.method == "POST":
-        max_id = max([int(customer["id"]) for customer in customers])
-        new_id = str(max_id + 1)
         name = request.form.get("name")
         email = request.form.get("email")
         policy = request.form.get("policy")
         start_date = request.form.get("startDate")
-        form_data = {
-            "id": new_id,
-            "name": name,
-            "email": email,
-            "policy": policy,
-            "startDate": start_date,
-        }
-        customers.append(form_data)
+        customers_data = Customer(
+            name=name,
+            email=email,
+            policy=policy,
+            startDate=start_date,
+        )
+        db.session.add(customers_data)
+        db.session.commit()
 
     for customer in customers:
         customer["years_difference"] = policy_length(customer["startDate"])
@@ -99,22 +136,29 @@ def customer_management():
 
 @app.route("/policy", methods=["GET"])
 def policy_page():
+    policies = Policy.query.all()
     return render_template("policy.html", policies=policies)
 
 
 @app.route("/claims", methods=["GET"])
-def claim_calculation():
+def claims_page():
+    policies = Policy.query.all()
+    customers = Customer.query.all()
 
     return render_template("claims.html", customers=customers, policies=policies)
 
 
 @app.route("/add_customer", methods=["GET"])
 def add_customer_page():
+    policies = Policy.query.all()
     return render_template("add_customer.html", policies=policies)
 
 
+# fix this part
 @app.route("/customer/<id>/delete")
 def delete_customer(id):
+    customers = Policy.query.get(id)
+
     selected_customer = next(
         (customer for customer in customers if customer["id"] == id), None
     )
@@ -134,3 +178,7 @@ def policy_length(start_date):
         years_difference -= 1
 
     return years_difference
+
+
+# claims claculations
+# def claims_calculation():
